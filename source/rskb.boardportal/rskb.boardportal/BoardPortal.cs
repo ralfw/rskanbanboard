2a72db1bae1c153675454a0ef9b37e415060de51
Ralf Westphal <info@ralfw.de>
2014-02-26 16:46:38 +0100

    BoardPortal auf WinForm umgestellt

--- source/rskb.boardportal/rskb.boardportal/BoardPortal.cs
+++ source/rskb.boardportal/rskb.boardportal/BoardPortal.cs
@@ -1,14 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 using rskb.contracts;
 
 namespace rskb.boardportal
 {
-    public class BoardPortal : IBoardPortal
+    public partial class BoardPortal : Form, IBoardPortal
     {
+        public BoardPortal()
+        {
+            InitializeComponent();
+        }
+
         public void Display_cards(IEnumerable<Card> cards)
         {
             throw new NotImplementedException();
