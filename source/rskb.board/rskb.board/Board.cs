6a595e9e54395f1ec937b8f096a6882db8fd3b83
Ralf Westphal <info@ralfw.de>
2014-02-26 17:40:09 +0100

    board.cs wiedergeholt

---
+++ source/rskb.board/rskb.board/Board.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using rskb.contracts;
+
+namespace rskb.board
+{
+    public class Board : IBoard
+    {
+        public Card Move_card_to_column(Card card, int destinationColumnIndex)
+        {
+            card.ColumnIndex = destinationColumnIndex;
+            return card;
+        }
+    }
+}
