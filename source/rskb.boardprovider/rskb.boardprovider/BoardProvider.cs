8fd4cacd8444b3392abdb40bcc1b3db2ab0e03a8
Pascal Wolff <pascal.wolff@gmail.com>
2014-02-26 17:27:02 +0100

    Added date for the sample board

--- source/rskb.boardprovider/rskb.boardprovider/BoardProvider.cs
+++ source/rskb.boardprovider/rskb.boardprovider/BoardProvider.cs
@@ -9,7 +9,18 @@ namespace rskb.boardprovider
 {
     public class BoardProvider : IBoardProvider
     {
-        private IEnumerable<Card> board = new[] { new Card() { ColumnIndex = 1, Id = "I1", Text = "A" } };
+        private IEnumerable<Card> board = new List<Card>()
+                                          {
+                                              new Card() { ColumnIndex = 0, Id = "0", Text = "A" },
+                                              new Card() { ColumnIndex = 0, Id = "1", Text = "B" },
+                                              new Card() { ColumnIndex = 0, Id = "2", Text = "C" },
+                                              new Card() { ColumnIndex = 1, Id = "3", Text = "D" },
+                                              new Card() { ColumnIndex = 1, Id = "4", Text = "E" },
+                                              new Card() { ColumnIndex = 2, Id = "5", Text = "F" },
+                                              new Card() { ColumnIndex = 3, Id = "6", Text = "G" },
+                                              new Card() { ColumnIndex = 3, Id = "7", Text = "H" },
+                                              new Card() { ColumnIndex = 3, Id = "8", Text = "I" },
+                                          };
 
         public IEnumerable<Card> Load_all_cards()
         {
@@ -18,7 +29,7 @@ namespace rskb.boardprovider
 
         public Card LoadCard(string id)
         {
-            return board.First();
+            return board.First(c => c.Id == id);
         }
 
         public void StoreCard(Card card)
