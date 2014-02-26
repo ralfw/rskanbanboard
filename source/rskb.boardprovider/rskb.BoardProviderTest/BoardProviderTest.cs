da9eda52cf128f539104c0de897f6570a9d18282
Pascal Wolff <pascal.wolff@gmail.com>
2014-02-26 17:23:08 +0100

    Initial test project

---
+++ source/rskb.boardprovider/rskb.BoardProviderTest/BoardProviderTest.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace rskb.BoardProviderTest
+{
+    using System.Linq;
+
+    using rskb.boardprovider;
+    using rskb.contracts;
+
+    [TestClass]
+    public class BoardProviderTest
+    {
+        [TestMethod]
+        public void LoadAllCards()
+        {
+            var provider = new BoardProvider();
+            var board = provider.Load_all_cards();
+            Assert.IsTrue(board.Any());
+        }
+
+        [TestMethod]
+        public void LoadCardById()
+        {
+            var provider = new BoardProvider();
+            var board = provider.Load_all_cards();
+            var expected = board.First();
+            var actual = provider.LoadCard(expected.Id);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void UpdateExistingCard()
+        {
+            var provider = new BoardProvider();
+            var board = provider.Load_all_cards();
+            var expected = board.First();
+            expected.ColumnIndex = 42;
+            provider.StoreCard(expected);
+            var actual = provider.LoadCard(expected.Id);
+            Assert.AreEqual(42, actual.ColumnIndex);
+        }
+
+        [TestMethod]
+        public void StoreNewCard()
+        {
+            var provider = new BoardProvider();
+            var expected = new Card() { ColumnIndex = 12345, Id = "qwertz", Text = "asdfghjkl" };
+            provider.StoreCard(expected);
+            var new_board = provider.Load_all_cards();
+            var card_from_board = new_board.Single(c => c.Id == expected.Id);
+            Assert.AreEqual(expected, card_from_board);
+
+            var loaded_card = provider.LoadCard(expected.Id);
+            Assert.AreEqual(expected, loaded_card);
+        }
+
+        [TestMethod]
+        public void UpdateCardWithoutStore_ShallNotUpdate()
+        {
+            var provider = new BoardProvider();
+            var board = provider.Load_all_cards();
+            var expected = board.First();
+            expected.ColumnIndex = 42;
+            var actual = provider.LoadCard(expected.Id);
+            Assert.AreNotEqual(42, actual.ColumnIndex);
+        }
+    }
+}
