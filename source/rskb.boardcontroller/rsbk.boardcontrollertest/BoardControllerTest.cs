b768be4922c1d7dae37716ea72560713d0de6c26
ruehland <ste.rue@web.de>
2014-02-27 10:35:24 +0100

    add unittest

--- source/rskb.boardcontroller/rsbk.boardcontrollertest/BoardControllerTest.cs
+++ source/rskb.boardcontroller/rsbk.boardcontrollertest/BoardControllerTest.cs
@@ -1,14 +1,26 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using rskb.boardcontroller;
+using rskb.contracts;
 
-namespace rsbk.boardcontrollertest
+namespace rsbk.boardcontroller
 {
     [TestClass]
     public class BoardControllerTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public void Move_Card_Test()
         {
+            IBoardProvider boardProvider = new BoardProviderMoc();
+            IBoardPortal bandPortal = new BoardPortalMoc();
+            IBoard board = new BoardMoc();
+            var boardController = new BoardController(board, boardProvider, bandPortal);
+            
+            String cardId = "A";
+            //// boardcontroller.Move_card(cardId, 2);
+            var card = boardProvider.LoadCard("A");
+
+            Assert.AreEqual(card.ColumnIndex, 2, "columIndex is incorrect");
         }
     }
 }
