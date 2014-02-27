6b684ade4767a49e1eef35a6758f2d25993eb68e
Pascal Wolff <pascal.wolff@gmail.com>
2014-02-27 10:59:34 +0100

    Added a persistent board provider using an XML file

---
+++ source/rskb.boardprovider/rskb.BoardProviderTest/PersistentBoardProviderTest.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace rskb.BoardProviderTest
+{
+    using System.IO;
+    using System.Linq;
+    using rskb.boardprovider;
+    using rskb.contracts;
+
+    [TestClass]
+    public class PersistentBoardProviderTest
+    {
+        private const String testFilename = "Testdata.xml";
+
+        private IBoardProvider provider;
+
+        [TestInitialize]
+        public void CreateTestData()
+        {
+            this.provider = new PersistentBoardProvider(testFilename);
+            provider.StoreCard(new Card(){Id = "0", ColumnIndex = 0, Text = "AAA"});
+            provider.StoreCard(new Card(){Id = "1", ColumnIndex = 1, Text = "BBB"});
+            provider.StoreCard(new Card(){Id = "2", ColumnIndex = 1, Text = "CCC"});
+            provider.StoreCard(new Card(){Id = "3", ColumnIndex = 2, Text = "DDD"});
+        }
+
+        [TestCleanup]
+        public void DeleteTestData()
+        {
+            File.Delete(testFilename);
+        }
+
+        [TestMethod]
+        public void LoadAllCards()
+        {
+            var board = this.provider.Load_all_cards();
+            Assert.IsTrue(board.Any());
+        }
+
+        [TestMethod]
+        public void LoadCardById()
+        {
+            var board = provider.Load_all_cards();
+            var expected = board.First();
+            var actual = provider.LoadCard(expected.Id);
+            Assert.AreEqual(expected.Id, actual.Id);
+        }
+
+        [TestMethod]
+        public void UpdateExistingCard()
+        {
+            var board = provider.Load_all_cards();
+            var expected = board.First();
+            expected.ColumnIndex = 42;
+            provider.StoreCard(expected);
+            var actual = provider.LoadCard(expected.Id);
+            Assert.AreEqual(42, actual.ColumnIndex);
+        }
+
+        [TestMethod]
+        public void StoreNewCard()
+        {
+            var expected = new Card() { ColumnIndex = 12345, Id = "qwertz", Text = "asdfghjkl" };
+            provider.StoreCard(expected);
+            var new_board = provider.Load_all_cards();
+            var card_from_board = new_board.Single(c => c.Id == expected.Id);
+            Assert.AreEqual(expected.Id, card_from_board.Id);
+
+            var loaded_card = provider.LoadCard(expected.Id);
+            Assert.AreEqual(expected.Id, loaded_card.Id);
+        }
+
+        [TestMethod]
+        public void UpdateCardWithoutStore_ShallNotUpdate()
+        {
+            var board = provider.Load_all_cards();
+            var expected = board.First();
+            expected.ColumnIndex = 42;
+            var actual = provider.LoadCard(expected.Id);
+            Assert.AreNotEqual(42, actual.ColumnIndex);
+        }
+    }
+}
---
+++ source/rskb.boardprovider/rskb.boardprovider/PersistentBoardProvider.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Xml.Serialization;
+using rskb.contracts;
+
+namespace rskb.boardprovider
+{
+
+    public class PersistentBoardProvider : IBoardProvider
+    {
+        private List<Card> initData = new List<Card>()
+                                          {
+                                              new Card() { ColumnIndex = 0, Id = "0", Text = "A" },
+                                              new Card() { ColumnIndex = 0, Id = "1", Text = "B" },
+                                              new Card() { ColumnIndex = 0, Id = "2", Text = "C" },
+                                              new Card() { ColumnIndex = 1, Id = "3", Text = "D" },
+                                              new Card() { ColumnIndex = 1, Id = "4", Text = "E" },
+                                              new Card() { ColumnIndex = 2, Id = "5", Text = "F" },
+                                              new Card() { ColumnIndex = 3, Id = "6", Text = "G" },
+                                              new Card() { ColumnIndex = 3, Id = "7", Text = "H" },
+                                              new Card() { ColumnIndex = 3, Id = "8", Text = "I" },
+                                          };
+
+        private String filename;
+        private XmlSerializer serializer = new XmlSerializer(typeof(List<Card>));
+
+        public PersistentBoardProvider()
+        {
+            this.filename = Path.Combine(Path.GetTempPath(), "KanbanCards.xml");
+        }
+
+        public PersistentBoardProvider(String filename)
+        {
+            this.filename = filename;
+        }
+
+        public IEnumerable<Card> Load_all_cards()
+        {
+            if (!File.Exists(this.filename))
+            {
+                return new List<Card>(this.initData);
+            }
+
+            List<Card> board = null;
+            using (var stream = new StreamReader(this.filename))
+            {
+                board = (List<Card>)this.serializer.Deserialize(stream);
+            }
+
+            return board;
+        }
+
+        public Card LoadCard(string id)
+        {
+            var board = this.Load_all_cards();
+            var existing_card = board.First(c => c.Id == id);
+            return existing_card;
+        }
+
+        public void StoreCard(Card card)
+        {
+            var board = this.Load_all_cards().ToList();
+            var existing_card = board.FirstOrDefault(c => c.Id == card.Id);
+            if (existing_card == null)
+            {
+                board.Add(card);
+            }
+            else
+            {
+                existing_card.ColumnIndex = card.ColumnIndex;
+                existing_card.Text = card.Text;
+            }
+
+            using (var stream = new StreamWriter(this.filename))
+            {
+                this.serializer.Serialize(stream, board);
+            }
+        }
+    }
+}
