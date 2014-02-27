c24dc086d9127a91dae74c42ef3b75c5c332296d
dewiese <detlef.wiese@rohde-schwarz.com>
2014-02-27 10:26:27 +0100

    Mocs implemented

--- source/rskb.boardcontroller/rsbk.boardcontrollertest/BoardControllerTestMocs.cs
+++ source/rskb.boardcontroller/rsbk.boardcontrollertest/BoardControllerTestMocs.cs
@@ -12,25 +12,33 @@ namespace rsbk.boardcontroller
     {
         public Card Move_card_to_column(Card card, int destinationColumnIndex)
         {
-            throw new NotImplementedException();
+            card.ColumnIndex = destinationColumnIndex;
+            return card;
         }
     }
 
     public class BoardProviderMoc : IBoardProvider
     {
+        List<Card> cards = new List<Card>();
+
+        public BoardProviderMoc()
+        {
+            this.cards.Add(new Card { ColumnIndex = 0, Id = "A", Text = "Die A Karte"});
+            this.cards.Add(new Card { ColumnIndex = 1, Id = "C", Text = "Die C Karte" });
+        }
+        
         public IEnumerable<Card> Load_all_cards()
         {
-            throw new NotImplementedException();
+            return cards;
         }
 
         public Card LoadCard(string id)
         {
-            throw new NotImplementedException();
+            return cards.First(c => c.Id == id);
         }
 
         public void StoreCard(Card card)
         {
-            throw new NotImplementedException();
         }
     }
 
@@ -38,7 +46,6 @@ namespace rsbk.boardcontroller
     {
         public void Display_cards(IEnumerable<Card> cards)
         {
-            throw new NotImplementedException();
         }
 
         public event Action<string, int> On_card_moved;
