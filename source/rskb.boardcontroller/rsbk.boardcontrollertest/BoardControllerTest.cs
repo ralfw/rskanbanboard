536095e7235aab72c139738f5a86db37e9d873f4
dewiese <detlef.wiese@rohde-schwarz.com>
2014-02-27 10:07:17 +0100

    Unit test projects

---
+++ source/rskb.boardcontroller/rsbk.boardcontrollertest/BoardControllerTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace rsbk.boardcontrollertest
+{
+    [TestClass]
+    public class BoardControllerTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
