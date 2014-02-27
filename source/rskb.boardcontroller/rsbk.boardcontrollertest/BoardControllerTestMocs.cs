b2abe8aa40778cfa98953be5d794961840ae54ac
dewiese <detlef.wiese@rohde-schwarz.com>
2014-02-27 10:17:09 +0100

    Mocs introduced

--- source/rskb.boardcontroller/rsbk.boardcontrollertest/BoardControllerTestMocs.cs
+++ source/rskb.boardcontroller/rsbk.boardcontrollertest/BoardControllerTestMocs.cs
@@ -4,9 +4,43 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace rsbk.boardcontrollertest
+namespace rsbk.boardcontroller
 {
-    class BoardControllerTestMocs
+    using rskb.contracts;
+
+    class BoardMoc : IBoard
     {
+        public Card Move_card_to_column(Card card, int destinationColumnIndex)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class BoardProviderMoc : IBoardProvider
+    {
+        public IEnumerable<Card> Load_all_cards()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Card LoadCard(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void StoreCard(Card card)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class BoardPortalMoc : IBoardPortal
+    {
+        public void Display_cards(IEnumerable<Card> cards)
+        {
+            throw new NotImplementedException();
+        }
+
+        public event Action<string, int> On_card_moved;
     }
 }
